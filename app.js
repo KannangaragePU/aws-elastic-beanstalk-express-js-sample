e857fc2cea5eb995655e772d03e7525813770d29
Pasindu <it17382304@my.sliit.lk>
2021-03-08 17:14:02 +0000

    mannaul approval

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! from Git Hub'));
+app.get('/', (req, res) => res.send('Hello World! from Git Hub with Mannual review CICD'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
